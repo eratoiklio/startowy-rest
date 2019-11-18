8fb07af97a7d05b0c45c18205dd847fd05df1fa4
Ada <eratoiklio@gmail.com>
2019-11-18 20:34:34 +0100

    start projektu

---
+++ src/main/java/com/ada/startowyrest/StartowyRestApplication.java
@@ -0,0 +1,13 @@
+package com.ada.startowyrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartowyRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartowyRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ada/startowyrest/StartowyRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.startowyrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartowyRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
